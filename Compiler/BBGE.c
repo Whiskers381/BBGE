ac474e45fbc7abdd15f819948408499bd024992f
Unknown <jamdw@hotmail.co.uk>
2020-07-26 23:59:14 +0100

    Change switch to if. C is not happy

--- Compiler/BBGE.c
+++ Compiler/BBGE.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	if (argc == 1)
+	{
+		/*run as an interpreture*/
+	}
+	else
+	{
+		//int keepProcessingArgs = 1;
+		int argvI = 1;
+		while (argvI < argc)
+		{
+			switch (argv[argvI])
+			{
+				case "-o":
+					if (++argvI <= argc)
+					{
+						printf("%s\n", argv[argvI]);
+					}
+					else
+					{
+						printf("missing <fileName> in option -o <fileName>");
+					}
+					break;
+			}
+			argvI++;
+		}
+		
+	}
+
+
+	return 0;
 }
